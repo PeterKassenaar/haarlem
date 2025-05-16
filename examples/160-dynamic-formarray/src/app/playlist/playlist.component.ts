e29d8efc20c4935b74744055d87e674ead7cfea2
PeterKassenaar <info@kassenaar.com>
2025-05-16 16:10:00 +0200

    feat: add example on dynamic formarray

---
+++ examples/160-dynamic-formarray/src/app/playlist/playlist.component.ts
@@ -0,0 +1,48 @@
+import {Component} from '@angular/core';
+import {FormArray, FormBuilder, Validators} from "@angular/forms";
+import {Playlist} from "../shared/model/playlist.interface";
+
+@Component({
+  selector: 'app-playlist',
+  templateUrl: './playlist.component.html'
+})
+export class PlaylistComponent {
+
+  // The typed form
+  playlist = this.fb.group({
+    title: ['', Validators.required],
+    date: [new Date(), Validators.required],
+    description: [''],
+    songs: this.fb.array([])
+  })
+  // For displaying in the UI
+  playlistRaw?: Playlist;
+
+  constructor(private fb: FormBuilder) {
+  }
+
+
+  get songs(): FormArray {
+    return this.playlist.controls['songs'] as FormArray
+  }
+
+  // Adding a new song by pushing a new song item to the formArray
+  addSong() {
+    const newSong = this.fb.group({
+      artist: ['', Validators.required],
+      title: ['', Validators.required],
+      duration: ['00:00', Validators.required]
+    })
+    this.songs.push(newSong);
+  }
+
+  // delete a song
+  deleteSong(index: number) {
+    this.songs.removeAt(index);
+  }
+
+  save() {
+    this.playlistRaw = this.playlist.getRawValue() as Playlist;
+    console.log('TODO: Post to database: ', this.playlist.getRawValue());
+  }
+}
